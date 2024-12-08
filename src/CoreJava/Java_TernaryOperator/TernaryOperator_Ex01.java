7f3a5b938fb91efa523452f19380707fe3c2f458
naresh <nareshmorkhade1703@gmail.com>
2024-12-08 22:29:53 +0530

    Commit all

---
+++ src/CoreJava/Java_TernaryOperator/TernaryOperator_Ex01.java
@@ -0,0 +1,9 @@
+package CoreJava.Java_TernaryOperator;
+
+public class TernaryOperator_Ex01 {
+    public static void main(String[] args) {
+        int number = -5;
+        String results = number > 0 ? "positive" : "Negative";
+        System.out.println(results);
+    }
+}
---
+++ src/CoreJava/Java_TernaryOperator/TernaryOperator_Ex02.java
@@ -0,0 +1,13 @@
+package CoreJava.Java_TernaryOperator;
+
+public class TernaryOperator_Ex02 {
+    public static void main(String[] args) {
+        int age = 19;
+        System.out.println( age>18 ? "Allow to vote" : "Not allow to vote");
+        /* you can do in string as we
+         ex: String result = age>18 ? "Allow to vote" : "Not allow to vote";
+             System.out.println(result);
+         */
+
+    }
+}
